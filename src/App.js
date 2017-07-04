21a45eeadf6e6442356888ec3a42b21f35a4d713
Mike Kornelson <mike@thrconsulting.ca>
2017-07-04 15:13:44 -0500

    * Switch to Jest/Enzyme instead of Mocha/Chai/Enzyme/Sinon

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Grid, Header, Form, Segment} from 'semantic-ui-react';
 import {MaskedInput, RadioGroup} from './dist';
 
-class App extends Component {
-  render() {
-    return (
-    	<Grid container>
-		    <Grid.Row>
-			    <Grid.Column>
-				    <Header as="h1">Controls Demo</Header>
-			    </Grid.Column>
-		    </Grid.Row>
-		    <Grid.Row>
-			    <Grid.Column>
-				    <Segment>
-					    <Form>
-						    <Form.Field>
-							    <label>MaskedInput</label>
-							    <MaskedInput mask={{mask: '99-999-99', autoUnmask: true, showMaskOnHover: false}}/>
-						    </Form.Field>
-						    <Form.Field>
-							    <label>RadioGroup</label>
-							    <RadioGroup grouped>
-								    <Form.Radio label="Option 1" value="1"/>
-								    <Form.Radio label="Option 2" value="2"/>
-							    </RadioGroup>
-						    </Form.Field>
-					    </Form>
-				    </Segment>
-			    </Grid.Column>
-		    </Grid.Row>
-	    </Grid>
-    );
-  }
+export default function App() {
+	return (
+		<Grid container>
+			<Grid.Row>
+				<Grid.Column>
+					<Header as="h1">Controls Demo</Header>
+				</Grid.Column>
+			</Grid.Row>
+			<Grid.Row>
+				<Grid.Column>
+					<Segment>
+						<Form>
+							<Form.Field>
+								<label>MaskedInput</label>
+								<MaskedInput mask={{mask: '99-999-99', autoUnmask: true, showMaskOnHover: false}}/>
+							</Form.Field>
+							<Form.Field>
+								<label>RadioGroup</label>
+								<RadioGroup grouped>
+									<Form.Radio label="Option 1" value="1"/>
+									<Form.Radio label="Option 2" value="2"/>
+								</RadioGroup>
+							</Form.Field>
+						</Form>
+					</Segment>
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	);
 }
-
-export default App;
